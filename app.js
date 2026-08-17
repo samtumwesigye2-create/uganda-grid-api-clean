d53f408bd672a47024cf2af3e16ec25650f93c15
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-08-17 05:15:55 -0400

    Implement navigateToAddress for route fetching
    Add navigateToAddress function to fetch and display route

--- app.js
+++ app.js
@@ -155,3 +155,55 @@ document
 
 
 });
+function navigateToAddress(destLat, destLng) {
+
+    navigator.geolocation.getCurrentPosition(function(position){
+
+        let startLat = position.coords.latitude;
+        let startLng = position.coords.longitude;
+
+        let url =
+        "https://router.project-osrm.org/route/v1/driving/" +
+        startLng + "," + startLat + ";" +
+        destLng + "," + destLat +
+        "?overview=full&geometries=geojson";
+
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            let route = data.routes[0].geometry;
+
+            if (map.getSource("route")) {
+                map.removeLayer("route");
+                map.removeSource("route");
+            }
+
+            map.addSource("route", {
+                type: "geojson",
+                data: {
+                    type: "Feature",
+                    geometry: route
+                }
+            });
+
+            map.addLayer({
+                id: "route",
+                type: "line",
+                source: "route",
+                paint: {
+                    "line-width": 5
+                }
+            });
+
+            alert(
+              "Distance: " +
+              (data.routes[0].distance / 1000).toFixed(1) +
+              " km"
+            );
+
+        });
+
+    });
+
+}
