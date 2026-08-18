47336304008bcacffbf715545cbb693ada6ac90a
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-08-17 22:00:34 -0400

    Refactor searchAddress function for improved readability

--- app.js
+++ app.js
@@ -3,28 +3,48 @@ const API = "https://uganda-grid-api-clean-production.up.railway.app";
 
 async function searchAddress(){
 
-    const query = document.getElementById("searchBox").value;
+    const query = document
+        .getElementById("searchBox")
+        .value;
+
 
     const response = await fetch(
         `${API}/search?q=${encodeURIComponent(query)}`
     );
 
+
     const data = await response.json();
 
 
-    const table = document.getElementById("results");
+    const results = document.getElementById("results");
 
-    table.innerHTML = "";
+
+    results.innerHTML = "";
 
 
     data.results.forEach(place => {
 
-        table.innerHTML += `
-            <tr>
-                <td>${place.grid_id || place.id || ""}</td>
-                <td>${place.code || place.street || ""}</td>
-                <td>${place.address || ""}</td>
-            </tr>
+
+        results.innerHTML += `
+
+        <tr>
+
+            <td>
+                ${place.grid_id || place.id || ""}
+            </td>
+
+
+            <td>
+                ${place.code || place.street || ""}
+            </td>
+
+
+            <td>
+                ${place.address || ""}
+            </td>
+
+        </tr>
+
         `;
 
     });
