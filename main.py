9a602b9c5a77303a89753e1535bed0a5a81cedc0
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-08-18 01:27:55 -0400

    Rename database file to entebbe_database.json

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE = os.path.join(
     BASE_DIR,
-    "uganda_national_grid_addresses_v2.json"
+    "entebbe_database.json"
 )
 
 
