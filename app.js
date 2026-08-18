73ca5ba2e1d771ec6d4453db120ccf8af27c40f5
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-08-17 22:12:19 -0400

    Refactor searchAddress function for clarity

--- app.js
+++ app.js
@@ -4,37 +4,28 @@ async function searchAddress() {
 
     const query = document.getElementById("searchBox").value;
 
-    const url = `${API}/search?q=${encodeURIComponent(query)}`;
-
-    console.log("Searching:", url);
-
-    const response = await fetch(url);
+    const response = await fetch(
+        `${API}/search?q=${encodeURIComponent(query)}`
+    );
 
     const data = await response.json();
-
-    console.log(data);
 
     const table = document.getElementById("results");
 
     table.innerHTML = "";
 
-    if (!data.results || data.results.length === 0) {
-        table.innerHTML = `
-        <tr>
-            <td colspan="3">No results found</td>
-        </tr>`;
-        return;
-    }
-
     data.results.forEach(place => {
 
-        table.innerHTML += `
+        const row = `
         <tr>
-            <td>${place.grid_id || ""}</td>
-            <td>${place.street || ""}</td>
-            <td>${place.address || ""}</td>
+            <td>${place.grid_id}</td>
+            <td>${place.street}</td>
+            <td>${place.address}</td>
         </tr>
         `;
 
+        table.innerHTML += row;
+
     });
+
 }
