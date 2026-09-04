a396aef6bd8625a5dfdd7af146c23132fe2aa78c
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-04 18:08:18 -0400

    Add WMS400Vector compatibility namespace

---
+++ wms400vector/__init__.py
@@ -0,0 +1,6 @@
+"""WMS400Vector corporate live system-of-record namespace."""
+
+SYSTEM_NAME = "WMS400Vector"
+SYSTEM_TYPE = "Corporate Live System of Record"
+ACCESS_CLASSIFICATION = "CORPORATE_ONLY"
+LEGACY_IMPLEMENTATION = "VECTOR5250"
