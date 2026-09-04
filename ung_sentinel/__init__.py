23f47b3941cc7cf8b9e1b52f17b8a51faa213e4e
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-04 18:08:07 -0400

    Add UNG Sentinel compatibility namespace

---
+++ ung_sentinel/__init__.py
@@ -0,0 +1,8 @@
+"""UNG Sentinel security platform namespace.
+
+This package provides a stable platform identity while existing production
+security modules remain in place for backwards compatibility.
+"""
+
+PLATFORM_NAME = "UNG Sentinel"
+PLATFORM_TYPE = "Security Monitoring & Protection Platform"
