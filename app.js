674c246f576d13974b4c069c11e9ea49a67285d0
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-08 05:58:48 -0400

    Load visible ZIPPER map layer before app core

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // UGAMAP bootstrap: active 5-digit ZIPPER only.
 (function(){
-  const RELEASE='20260902-5digit-r3';
+  const RELEASE='20260908-zip-visible-r1';
   try{
     if('caches' in window){
       caches.keys().then(keys=>Promise.all(keys.filter(k=>/^ugamap-/i.test(k)).map(k=>caches.delete(k)))).catch(()=>{});
@@ -8,5 +8,6 @@
   }catch(_){}
   document.write('<script src="/boundaries.js?v='+RELEASE+'"><\/script>');
   document.write('<script src="/performance-layer.js?v=5-'+RELEASE+'"><\/script>');
+  document.write('<script src="/zip-map-layer.js?v='+RELEASE+'"><\/script>');
   document.write('<script src="/app-core.js?v=9-'+RELEASE+'"><\/script>');
 })();
